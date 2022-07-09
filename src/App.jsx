ad640c73692836f3677800fb6b66e197da0eb7b2
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2022-07-10 00:26:21 +0530

    Income & Expenses created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
-
+import { Grid } from '@material-ui/core';
+import useStyles from './styles';
+import { Details } from './components';
 function App() {
+  const classes = useStyles();
   return (
     <>
-      This is Audit Assistant.
+      <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh' }}>
+        <Grid item xs={12} sm={4} className={classes.mobile}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3} className={classes.main}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.desktop}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={4} className={classes.last}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
     </>
   );
 }
